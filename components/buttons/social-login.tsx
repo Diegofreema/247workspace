02d13101104ff6b08039869837dc634f22b96432
diego <diegofreeman78@gmail.com>
2025-04-24 03:06:56 +0100

    refactor(buttons): extract submit button logic into reusable component
    The SubmitButton component was created to encapsulate the common logic for social login buttons, reducing code duplication and improving maintainability. This change simplifies the SocialLogin component by replacing repetitive button code with the new SubmitButton component.

--- components/buttons/social-login.tsx
+++ components/buttons/social-login.tsx
@@ -1,44 +1,15 @@
 import { signUpWithGithub, signUpWithGoogle } from '@/lib/oauth';
 import { Button, Image, Stack } from '@chakra-ui/react';
+import { SubmitButton } from './submit-button';
 
 export const SocialLogin = () => {
   return (
     <Stack gap={4} width={'100%'}>
       <form action={signUpWithGoogle} className="w-full">
-        <Button
-          variant={'outline'}
-          width={'100%'}
-          border={'1px solid #ccc'}
-          color="black"
-          type="submit"
-        >
-          <Image
-            alt="google image"
-            src="/google.png"
-            width={25}
-            height={25}
-            objectFit={'contain'}
-          />
-          Google
-        </Button>
+        <SubmitButton text="Google" url="/google.png" />
       </form>
       <form action={signUpWithGithub} className="w-full">
-        <Button
-          variant={'outline'}
-          width={'100%'}
-          color="black"
-          border={'1px solid #ccc'}
-          type="submit"
-        >
-          <Image
-            alt="github image"
-            src="/github.png"
-            width={25}
-            height={25}
-            objectFit={'contain'}
-          />
-          Github
-        </Button>
+        <SubmitButton text="Github" url="/github.png" />
       </form>
     </Stack>
   );
---
+++ components/buttons/submit-button.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { LoaderCircle } from 'lucide-react';
+import Image from 'next/image';
+import { useFormStatus } from 'react-dom';
+import { Button } from '../custom/custom-button';
+
+type Props = {
+  url: string;
+  text: string;
+};
+
+export const SubmitButton = ({ text, url }: Props) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      variant={'outline'}
+      width={'100%'}
+      border={'1px solid #ccc'}
+      color="black"
+      type="submit"
+      disabled={pending}
+    >
+      {pending ? (
+        <LoaderCircle className="animate-spin size-5 text-purple" />
+      ) : (
+        <Image
+          alt="google image"
+          src={url}
+          width={25}
+          height={25}
+          objectFit={'contain'}
+        />
+      )}
+
+      {text}
+    </Button>
+  );
+};
